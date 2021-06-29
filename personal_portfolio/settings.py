ec8196b2c6ad1dfdef78aa3c2491a55126b3c21f
Bargavi <bargaviiu@gmail.com>
2021-06-29 13:38:09 +0000

    Remove old data

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0p)up2^12q=%f)8k_6s$j==9qdl4otr%e%+@=86g=(+os!&%b5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bargavi.pythonanywhere.com']
 
 
 # Application definition
